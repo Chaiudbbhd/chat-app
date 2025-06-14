49756a5f5d7bd71c5ce9cd79741aea607bc2dd9f
Lakshmi prasanna kumar <125391606+Chaiudbbhd@users.noreply.github.com>
2025-06-14 22:05:59 +0530

    Update ChatRoom.js

--- server/models/ChatRoom.js
+++ server/models/ChatRoom.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
-const ChatRoomSchema = mongoose.Schema(
+const ChatRoomSchema = new mongoose.Schema(
   {
-    members: Array,
+    members: { type: [String], required: true },
   },
   { timestamps: true }
 );
 
 const ChatRoom = mongoose.model("ChatRoom", ChatRoomSchema);
-
 export default ChatRoom;
