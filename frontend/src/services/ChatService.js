48d76a1318ee001a4e1d2820a8df9aa671ae0e7d
Lakshmi prasanna kumar <125391606+Chaiudbbhd@users.noreply.github.com>
2025-06-14 22:31:38 +0530

    Update ChatService.js

--- frontend/src/services/ChatService.js
+++ frontend/src/services/ChatService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import auth from "../config/firebase";
 import { io } from "socket.io-client";
 
-const baseURL = "http://localhost:3001/api";
+// 🌟 Set your deployed backend URL (Render)
+const baseURL = "https://chat-app-1-euxt.onrender.com/api";
 
 const getUserToken = async () => {
   const user = auth.currentUser;
@@ -13,7 +14,8 @@ const getUserToken = async () => {
 export const initiateSocketConnection = async () => {
   const token = await getUserToken();
 
-  const socket = io("http://localhost:3001", {
+  const socket = io("https://chat-app-1-euxt.onrender.com", {
+    withCredentials: true,
     auth: {
       token,
     },
