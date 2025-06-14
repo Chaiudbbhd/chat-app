fcaaeae33cec1f0cabe52586f0f3395c41b37e41
Lakshmi prasanna kumar <125391606+Chaiudbbhd@users.noreply.github.com>
2025-06-14 18:39:24 +0530

    Update VerifyToken.js

--- server/middlewares/VerifyToken.js
+++ server/middlewares/VerifyToken.js
@@ -1,31 +1,53 @@
 import auth from "../config/firebase-config.js";
 
 export const VerifyToken = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No Authorization header provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Token format is invalid" });
+    }
+
     const decodeValue = await auth.verifyIdToken(token);
+
     if (decodeValue) {
       req.user = decodeValue;
       return next();
+    } else {
+      return res.status(401).json({ message: "Invalid token" });
     }
+
   } catch (e) {
-    return res.json({ message: "Internal Error" });
+    console.error("Token verification error:", e);
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const VerifySocketToken = async (socket, next) => {
-  const token = socket.handshake.auth.token;
-
   try {
+    const token = socket.handshake.auth?.token;
+
+    if (!token) {
+      return next(new Error("No token provided in socket handshake"));
+    }
+
     const decodeValue = await auth.verifyIdToken(token);
 
     if (decodeValue) {
       socket.user = decodeValue;
-
       return next();
+    } else {
+      return next(new Error("Invalid token"));
     }
+
   } catch (e) {
-    return next(new Error("Internal Error"));
+    console.error("Socket token verification error:", e);
+    return next(new Error("Internal server error"));
   }
 };
